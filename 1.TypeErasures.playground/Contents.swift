53fa6ab4746e2266db393e0e915251ac34cb9973
Saravanakumar S <saravanakumars@equalexperts.com>
2021-02-21 00:15:22 +0530

    Add Type Erasure Resource

---
+++ 1.TypeErasures.playground/Contents.swift
@@ -0,0 +1,68 @@
+
+protocol Vehicle {
+    var model: String { get }
+    var brand: String { get }
+    func isEqualTo(_ other: Vehicle) -> Bool
+    func asEquatable() -> EquatableVehicle
+}
+
+extension Vehicle where Self: Equatable {
+    func isEqualTo(_ other: Vehicle) -> Bool {
+        guard let otherVehicle = other as? Self else { return false }
+
+        return otherVehicle == self
+    }
+
+    func asEquatable() -> EquatableVehicle {
+        return EquatableVehicle(self)
+    }
+}
+
+
+struct EquatableVehicle: Vehicle {
+
+    var model: String {
+        return vehicle.model
+    }
+
+    var brand: String {
+        return vehicle.brand
+    }
+
+    private let vehicle: Vehicle
+
+    init(_ vehicle: Vehicle) {
+        self.vehicle = vehicle
+    }
+}
+
+extension EquatableVehicle: Equatable {
+    static func == (lhs: EquatableVehicle, rhs: EquatableVehicle) -> Bool {
+        return lhs.vehicle.isEqualTo(rhs.vehicle)
+    }
+}
+
+
+//Testing
+struct Hatchback: Vehicle, Equatable {
+    var model: String
+
+    var brand: String
+
+}
+
+struct Suv: Vehicle, Equatable {
+    var model: String
+
+    var brand: String
+
+}
+
+let hundaiI20 = Hatchback(model: "Hundai", brand: "i20")
+let marutiBaleno = Hatchback(model: "Maruti", brand: "baleno")
+let anotherHundaiI20 = Hatchback(model: "Hundai", brand: "i20")
+let anSuv = Suv(model: "Maruti", brand: "baleno")
+
+print(hundaiI20.asEquatable() == anotherHundaiI20.asEquatable())
+print(hundaiI20.asEquatable() == marutiBaleno.asEquatable())
+print(hundaiI20.asEquatable() == anSuv.asEquatable())
